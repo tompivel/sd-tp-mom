5438d9c7b0bb5788406a1d83fc4cca3e4cc2bc58
Tom Pinargote <tpinargote@fi.uba.ar>
2026-09-15 12:01:56 -0300

    base middleware, method for queue consumption

--- golang/internal/factory/base.go
+++ golang/internal/factory/base.go
@@ -36,3 +36,56 @@ func NewBaseMiddleware(settings m.ConnSettings) (*BaseMiddleware, error) {
 	}, nil
 }
 
+func (b *BaseMiddleware) StartConsumingQueue(queueName string, callbackFunc func(msg m.Message, ack func(), nack func())) error {
+	b.mu.Lock()
+	if b.isConsuming {
+		b.mu.Unlock()
+		return fmt.Errorf("already consuming")
+	}
+	
+	if b.conn.IsClosed() {
+		b.mu.Unlock()
+		return m.ErrMessageMiddlewareDisconnected
+	}
+
+	// unique consumer tag
+	b.consumerTag = fmt.Sprintf("consumer-%d", time.Now().UnixNano())
+	b.isConsuming = true
+	b.mu.Unlock()
+
+	msgs, err := b.ch.Consume(
+		queueName,
+		b.consumerTag, // consumer
+		false,         // auto-ack
+		false,         // exclusive
+		false,         // no-local
+		false,         // no-wait
+		nil,           // args
+	)
+	
+	if err != nil {
+		b.mu.Lock()
+		b.isConsuming = false
+		b.mu.Unlock()
+		
+		if b.conn.IsClosed() {
+			return m.ErrMessageMiddlewareDisconnected
+		}
+		return m.ErrMessageMiddlewareMessage
+	}
+
+	go func() {
+		for d := range msgs {
+			msg := m.Message{Body: string(d.Body)}
+			
+			delivery := d
+			ack := func() { delivery.Ack(false) }
+			nack := func() { delivery.Nack(false, true) }
+			
+			callbackFunc(msg, ack, nack)
+		}
+	}()
+
+	return nil
+}
+
