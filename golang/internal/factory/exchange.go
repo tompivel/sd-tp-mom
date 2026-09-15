8bee6f2d860630c03f1cb6a8b536d8b60e9d9f4f
Tom Pinargote <tpinargote@fi.uba.ar>
2026-09-15 12:09:28 -0300

    exchange middleware send method, publishing with context

--- golang/internal/factory/exchange.go
+++ golang/internal/factory/exchange.go
@@ -15,6 +15,40 @@ type ExchangeMiddleware struct {
 }
 
 func (e *ExchangeMiddleware) StartConsuming(callbackFunc func(msg m.Message, ack func(), nack func())) error {
+	// The queue is already created and bound during initialization.
 	return e.BaseMiddleware.StartConsumingQueue(e.queueName, callbackFunc)
 }
 
+func (e *ExchangeMiddleware) Send(msg m.Message) error {
+	if e.conn.IsClosed() {
+		return m.ErrMessageMiddlewareDisconnected
+	}
+
+	// TODO: If there is more than one routing key provided, send the same message to each routine key.
+	// TODO: PublishWithContext could be encapsulated inside a method for BaseMiddleware, maybe "PublishWithTimeout".
+
+	routingKey := ""
+	if len(e.routingKeys) > 0 {
+		routingKey = e.routingKeys[0]
+	}
+
+	err := e.ch.PublishWithContext(
+		context.Background(),
+		e.exchangeName, // exchange
+		routingKey,     // routing key
+		false,          // mandatory
+		false,          // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(msg.Body),
+		},
+	)
+	if err != nil {
+		if e.conn.IsClosed() {
+			return m.ErrMessageMiddlewareDisconnected
+		}
+		return m.ErrMessageMiddlewareMessage
+	}
+
+	return nil
+}
