90694d1a12b14676fb2cfd64d4995a10e86b414f
Tom Pinargote <tpinargote@fi.uba.ar>
2026-09-15 12:00:06 -0300

    base middleware struct type, lock for use between threads

--- golang/internal/factory/base.go
+++ golang/internal/factory/base.go
@@ -9,3 +9,11 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+type BaseMiddleware struct {
+	conn         *amqp.Connection
+	ch           *amqp.Channel
+	mu           sync.Mutex
+	isConsuming  bool
+	consumerTag  string
+}
+
