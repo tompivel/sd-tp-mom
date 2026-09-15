5e251047f657562ec0f79e40aae5cbcf5809842a
Tom Pinargote <tpinargote@fi.uba.ar>
2026-09-15 12:11:10 -0300

    implementing queue middleware factory

--- golang/internal/factory/factory.go
+++ golang/internal/factory/factory.go
@@ -5,7 +5,28 @@ import (
 )
 
 func CreateQueueMiddleware(queueName string, connectionSettings m.ConnSettings) (m.Middleware, error) {
-	return nil, nil
+	base, err := NewBaseMiddleware(connectionSettings)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = base.ch.QueueDeclare(
+		queueName,
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	if err != nil {
+		base.Close()
+		return nil, m.ErrMessageMiddlewareDisconnected
+	}
+
+	return &QueueMiddleware{
+		BaseMiddleware: base,
+		queueName:      queueName,
+	}, nil
 }
 
 func CreateExchangeMiddleware(exchange string, keys []string, connectionSettings m.ConnSettings) (m.Middleware, error) {
