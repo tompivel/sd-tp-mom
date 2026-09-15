095958405d133ae9378bdd4712f9350383d86e3a
Tom Pinargote <tpinargote@fi.uba.ar>
2026-09-15 12:04:14 -0300

    queue middleware type definition, composing base middleware

---
+++ golang/internal/factory/queue.go
@@ -0,0 +1,14 @@
+package factory
+
+import (
+	"context"
+
+	m "github.com/7574-sistemas-distribuidos/tp-mom/golang/internal/middleware"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type QueueMiddleware struct {
+	*BaseMiddleware
+	queueName string
+}
+
