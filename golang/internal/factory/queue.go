bd2728018bf1c4da2cffa6d3a3f6e7cee07c19a1
Tom Pinargote <tpinargote@fi.uba.ar>
2026-09-15 12:07:13 -0300

    queue middleware send method, publishing with context

--- golang/internal/factory/queue.go
+++ golang/internal/factory/queue.go
@@ -16,3 +16,31 @@ func (q *QueueMiddleware) StartConsuming(callbackFunc func(msg m.Message, ack fu
 	return q.BaseMiddleware.StartConsumingQueue(q.queueName, callbackFunc)
 }
 
+func (q *QueueMiddleware) Send(msg m.Message) error {
+	if q.conn.IsClosed() {
+		return m.ErrMessageMiddlewareDisconnected
+	}
+
+	//TODO: Define constant for default exchange
+	//TODO: Create context with 5s timeout
+	//TODO: Persistent messages?
+	err := q.ch.PublishWithContext(
+		context.Background(),
+		"",          // exchange
+		q.queueName, // routing key
+		false,       // mandatory
+		false,       // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(msg.Body),
+		},
+	)
+	if err != nil {
+		if q.conn.IsClosed() {
+			return m.ErrMessageMiddlewareDisconnected
+		}
+		return m.ErrMessageMiddlewareMessage
+	}
+
+	return nil
+}
